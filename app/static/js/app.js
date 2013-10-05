4c8a50c3621958a4ffdddfcb08b9f419f56fa58a
Kevin Wang <kevmo314@gmail.com>
2013-10-04 19:44:21 -0500

    add searching
    lolz

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -3,6 +3,7 @@ function Ctrl($scope, $http) {
 	$scope.search = function() {
 		$http.get('/places/search/', {q:$scope.query})
 			.success(function(data, status, headers, config) {
+				$scope.places = data;
 			})
 	};
 }
