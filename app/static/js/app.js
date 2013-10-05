4ece807497babeb5b2bba604a8b138b56298de5a
Kevin Wang <kevmo314@gmail.com>
2013-10-04 20:17:56 -0500

    add mapping
    mapping

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,9 +1,21 @@
 var app = angular.module('app', []);
 function Ctrl($scope, $http) {
+	angular.extend($scope, {
+		center: {
+			latitude: 0,
+			longitude: 0
+		},
+		markers: [],
+		zoom: 8
+	});
 	$scope.search = function() {
 		$http.get('/places/search/', {q:$scope.query})
 			.success(function(data, status, headers, config) {
 				$scope.places = data;
+				$scope.markers = [];
+				for(var place in data) {
+					$scope.markers.push({latitude:place.lat, longitude:place.lng});
+				}
 			})
 	};
 }
